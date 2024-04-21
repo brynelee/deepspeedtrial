39cae1a366f85e23910dfe781571de9bc95b80de
Bryne Lee <bryne_lxd@sina.com>
2024-04-21 21:28:58 +0800

    added cmd_demo.sh

--- deepspeed_train_demo/cmd_demos.sh
+++ deepspeed_train_demo/cmd_demos.sh
@@ -4,5 +4,9 @@ deepspeed ds_train.py --epoch 1 --deepspeed --deepspeed_config ds_config.json
 # 本地推理
 deepspeed ds_eval.py --deepspeed --deepspeed_config ds_config.json
 
-# 远程训练
+# 分布式训练
 deepspeed --hostfile hostfile ds_train.py --epoch 1 --deepspeed --deepspeed_config ds_config.json
+
+# 分布式推理
+deepspeed --hostfile hostfile ds_eval.py --deepspeed --deepspeed_config ds_config.json
+
